c1d8d1eed0eddc29183d83755743764a4586871f
Suparna Khamaru <suparna3107@gmail.com>
2020-05-09 20:38:08 +0530

    removed unused list

--- ReverseArrayWithoutCreatingAdditionalArray.swift
+++ ReverseArrayWithoutCreatingAdditionalArray.swift
@@ -4,7 +4,7 @@ import Foundation
 
 var listOfNumbers = [1,2,3,10,100]
 
-func reverseArray(list: [Int]) -> [Int] {
+func reverseArray() -> [Int] {
     
     var firstItem = 0
     var lastItem = listOfNumbers.count-1
@@ -21,53 +21,7 @@ func reverseArray(list: [Int]) -> [Int] {
     return listOfNumbers
 }
 
-print(reverseArray(list: listOfNumbers)). //import Foundation
-
-//Reverse the order of the elements in listOfNumbers without creating any additional arrays
-
-var listOfNumbers = [1,2,3,10,100]
-
-func reverseArray(list: [Int]) -> [Int] {
-    
-    var firstItem = 0
-    var lastItem = listOfNumbers.count-1
-    
-    while firstItem < lastItem {
-        
-        var temp = listOfNumbers[firstItem]
-        listOfNumbers[firstItem] = listOfNumbers[lastItem]
-        listOfNumbers[lastItem] = temp
-        
-        firstItem = firstItem + 1
-        lastItem = lastItem - 1
-    }
-    return listOfNumbers
-}
-
-print(reverseArray(list: listOfNumbers)) //import Foundation
-
-//Reverse the order of the elements in listOfNumbers without creating any additional arrays
-
-var listOfNumbers = [1,2,3,10,100]
-
-func reverseArray(list: [Int]) -> [Int] {
-    
-    var firstItem = 0
-    var lastItem = listOfNumbers.count-1
-    
-    while firstItem < lastItem {
-        
-        var temp = listOfNumbers[firstItem]
-        listOfNumbers[firstItem] = listOfNumbers[lastItem]
-        listOfNumbers[lastItem] = temp
-        
-        firstItem = firstItem + 1
-        lastItem = lastItem - 1
-    }
-    return listOfNumbers
-}
-
-print(reverseArray(list: listOfNumbers))  //[100, 10, 3, 2, 1]
+print(reverseArray())   //[100, 10, 3, 2, 1]
 
 
 
