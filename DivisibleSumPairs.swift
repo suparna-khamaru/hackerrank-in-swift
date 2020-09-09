6781248037af8093799d04a8684077cf0bbe1324
Suparna Khamaru <suparna3107@gmail.com>
2020-09-09 23:50:06 +0530

    flow commented

--- DivisibleSumPairs.swift
+++ DivisibleSumPairs.swift
@@ -22,3 +22,13 @@ func divisibleSumPairs(n: Int, k: Int, ar: [Int]) -> Int {
 
 print(divisibleSumPairs(n: 6, k: 5, ar: [1,2,3,4,5,6]))  // 3
 print(divisibleSumPairs(n: 6, k: 3, ar: [1,3,2,6,1,2]))  // 5
+
+
+/*
+i = 0   ->  j = 1,2,3,4,5   (step: 1 to 5)
+i = 1   ->  j =   2,3,4,5   (step: 2 to 5)
+i = 2   ->  j =     3,4,5   (step: 3 to 5)
+i = 3   ->  j =       4,5   (step: 4 to 5)
+i = 4   ->  j =         5   (step: 5 to 5)
+i = 5
+*/
